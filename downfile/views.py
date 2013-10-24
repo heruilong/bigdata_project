f4755ead4f495454f3e84eaf532b08e52dae7f27
heruilong <heruilong@jhu.edu>
2013-10-24 23:04:49 +0800

    solve the js reference problem

--- downfile/views.py
+++ downfile/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from utility import fetdata
 
 def index(request):
+    print '999'
     return render(request, 'downfile/index.html');
 
 
@@ -13,5 +14,5 @@ def download(request):
     seqTypeVal = request.POST['seqType']
     #print clade + genome + seqType
     print cladeVal+"---"+genomeVal+"---"+seqTypeVal
-    fetdata.fetch_data_from_ucsc(clade = cladeVal, genome = genomeVal, seqType = seqTypeVal)
-    return HttpResponse("hellworld")
+    #fetdata.fetch_data_from_ucsc(clade = cladeVal, genome = genomeVal, seqType = seqTypeVal)
+    return HttpResponse("hellworld----")
