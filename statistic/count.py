bd79aa53a1727aacbaffe0f8f315bd1c430cec3c
xniu7 <nx.niuxiang@gmail.com>
2013-10-24 22:58:26 -0400

    stat

---
+++ statistic/count.py
@@ -0,0 +1,23 @@
+import sys
+import geneFormat
+
+# count the number of A C G T of each dna sequence
+def dnaCount(dna):
+        return (dna.count('A'),dna.count('C'),dna.count('G'),dna.count('T'))
+
+# count all A C G T of one fasta file.
+def fastaCount(fastaStr):
+	fastaArr = geneFormat.decodeFasta(fastaStr)
+	countRes = [0,0,0,0]
+	for dna in fastaArr[1::2]:
+		eachCount = dnaCount(dna)
+		for i in xrange(0,len(countRes)):
+			countRes[i] += eachCount[i]
+	return countRes
+
+# test
+def test():
+	fastaStr = sys.stdin.read()
+	print fastaCount(fastaStr)	
+	
+test()
